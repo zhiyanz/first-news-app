4a4c3cc1b6c279438cfccb2d9e9cbd466d8c5f8f
Zhiyan Zhong <zzhong05@syr.edu>
2017-03-04 16:19:19 -0500

    made detail page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import csv 
+import csv
+from flask import abort
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -16,6 +17,15 @@ def index():
 	template = "index.html"
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
+
+@app.route('/<row_id>/')
+def detail(row_id):
+	template = "detail.html"
+	object_list = get_csv()
+	for row in object_list:
+		if row['id'] == row_id:
+			return render_template(template, object=row)
+	abort(404)
 
 # if this script is run from the command line
 if __name__ == "__main__":
